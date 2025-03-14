ed94ea64a9c4e9802aea605ed27b9a80ff62a9d0
aluno <aluno@mac23.local>
2025-03-14 20:06:47 +0000

    add: session 4

---
+++ session_4/src/session_4/iterators.py
@@ -0,0 +1,32 @@
+class EvenIterator:
+    def __init__(self, numbers):
+        self.numbers = numbers
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # Use enumerate-like behaviour to track index and value
+        while self.index < len(self.numbers):
+            num = self.numbers[self.index]
+            self.index += 1
+            if num % 2 == 0:
+                return num
+        raise StopIteration
+
+# Using enumerate to show index-value pairs
+items = range(1,20)
+
+for index, num in enumerate(EvenIterator(items)):
+    print(f"even numbers {num}")
+
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+for num in fibonacci(10):
+    print("Fibonacci number:", num)
