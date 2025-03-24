e8984105b1789db24dda2624d42ddeb8eeaf610a
Nelson Zarate <nelson_zarate@eticalgarve.com>
2025-03-24 22:02:00 +0000

    add: tests and dockerfile

--- session_15/todo/views.py
+++ session_15/todo/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView,TemplateView,FormView
+from django.views.generic import TemplateView,FormView,CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import get_user
+from django.contrib.auth import logout 
+from django.shortcuts import redirect
+
+
+
+from todo.forms import TaskForm
 from todo.models import Task
 
 # Create your views here.
@@ -11,13 +16,20 @@ def index(request):
     tasks = Task.objects.all()
     return render(request,"todo/index.html", {"foo":"World", "tasks":tasks})
 
-class TaskListView(LoginRequiredMixin,ListView):
-    login_url = LoginRequiredMixin
-    model = Task
-    
-    def get_queryset(self):
-        return Task.objects.filter(user = self.request.user).all()
+class TaskListView(LoginRequiredMixin,CreateView):
+    login_url = "/signin"
+    success_url="/tasks"
+    form_class= TaskForm
+    template_name = "todo/task_list.html"
 
+    def get_context_data(self, **kwargs):
+
+        kwargs["object_list"] = Task.objects.filter(user = self.request.user).all()
+        return super().get_context_data(**kwargs)
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
     
 class IndexView(TemplateView):
     http_method_names = ['get']
@@ -31,3 +43,8 @@ class SignUpView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+def logout_view(request):   
+    if request.method == "POST":
+        logout(request)
+        return redirect("/")
